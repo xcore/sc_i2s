dc1c5df370a54c895739689071d8f87120e8e3d6
Russell <russell@xmos.com>
2012-02-07 15:42:34 +0000

    Add description of sample order

--- module_i2s_slave/src/i2s_slave.h
+++ module_i2s_slave/src/i2s_slave.h
@@ -36,14 +36,20 @@ struct i2s_slave {
 
 /** I2S Slave function
  *
- * Samples are returned left-aligned
+ * Samples are left-aligned signed values.
  * e.g. 24-bit audio will look like 0x12345600 (positive) or 0xFF123400 (negative)
  *
  * \param r_i2s_slave    Structure to configure the i2s_slave
  *
- * \param c_in           Input streaming channel for sample data
+ * \param c_in           Input streaming channel for sample data.
+ *                       Samples are returned in the following order:
+ *                       Left (din[0]), .., Left (din[I2S_SLAVE_NUM_IN - 1]),
+ *                       Right (din[0]), .., Right (din[I2S_SLAVE_NUM_IN - 1])
  *
  * \param c_out          Output streaming channel for sample data
+ *                       Samples should be sent in the following order:
+ *                       Left (dout[0]), .., Left (dout[I2S_SLAVE_NUM_OUT - 1]),
+ *                       Right (dout[0]), .., Right (dout[I2S_SLAVE_NUM_OUT - 1])
  */
 void i2s_slave(struct i2s_slave &r_i2s_slave, streaming chanend c_in, streaming chanend c_out);
 
