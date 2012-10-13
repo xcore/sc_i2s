6e6204151c62b17bd2436e7d30ee4e2ed7a6a387
David Lacey <david.lacey@xmos.com>
2012-10-13 16:04:41 +0100

    Added example conf file

---
+++ module_i2s_master/i2s_master_conf_example.h
@@ -0,0 +1,8 @@
+
+// Set this define to control the number of input
+// channels the I2S master component implements
+#define I2S_MASTER_NUM_CHANS_ADC 4
+
+// Set this define to control the number of output channels the
+// I2S master component implements
+#define I2S_MASTER_NUM_CHANS_DAC 4
